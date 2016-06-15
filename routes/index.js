d2cf0dd426e71b15e87d2ed258112624fde1fc3a
peterjostiguy <peterjostiguy@gmail.com>
2016-06-15 15:38:09 -0600

    working on handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('robot').select().then(function(robots){
+  res.render('index', { robots: robots });
+  })
 });
 
 module.exports = router;
