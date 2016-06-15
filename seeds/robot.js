c86b91bd0a2668c689f8b6da666ab96e1daf7e27
peterjostiguy <peterjostiguy@gmail.com>
2016-06-15 15:21:06 -0600

    updated seed, removed broken migration

--- seeds/robot.js
+++ seeds/robot.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
  return knex('robot').del().then(function(){
   return Promise.join(
 
-    knex('robot').insert({name: 'C3PO', alliance: "1", owner: 'Anikin'}),
-    knex('robot').insert({name: 'R2D2', alliance: "1", owner: 'Luke'}),
-    knex('robot').insert({name: 'BB-8', alliance: "1", owner: 'Rey'})
+    knex('robot').insert({name: 'C3PO', alliance: "1", owner: 'Anikin', description: 'humanoid, gold', image:'http://vignette2.wikia.nocookie.net/starwars/images/7/7e/ThreepioTFA-Fathead.png/revision/latest?cb=20150909022733'}),
+    knex('robot').insert({name: 'R2D2', alliance: "1", owner: 'Luke', description: 'Moving Trash Can, Beeps', image: 'http://vignette2.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png/revision/latest?cb=20150909022306'}),
+    knex('robot').insert({name: 'BB-8', alliance: "1", owner: 'Rey', description: 'round, orange, beeps', image: 'http://vignette2.wikia.nocookie.net/starwars/images/6/68/BB8-Fathead.png/revision/latest'})
 
     );
   })
